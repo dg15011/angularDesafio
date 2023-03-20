969e26122c3cab975fa90184ff3599b0ac17eb3a
dg15011 <126188486+dg15011@users.noreply.github.com>
2023-03-19 22:10:28 -0600

    Se Guardo los cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { PiePaginaComponent } from './pie-pagina/pie-pagina.component';
+import { BarraLateralComponent } from './barra-lateral/barra-lateral.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PiePaginaComponent,
+    BarraLateralComponent,
+    CuerpoComponent
   ],
   imports: [
     BrowserModule
